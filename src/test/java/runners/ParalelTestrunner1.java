7d342ef4c87a582f64809bb09c31da26389b7ad0
Halil Yavuz <halilibrahimyavuz07@gmail.com>
2023-01-17 10:15:51 +0300

    adw

--- src/test/java/runners/ParalelTestrunner1.java
+++ src/test/java/runners/ParalelTestrunner1.java
@@ -9,17 +9,18 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(
-        plugin={"html:target/cucumber-html-reports2/cucumber.html", // buradaki / \\ idi, degistirdim, emin degilim
-                "json:target/json-reports/cucumber2.json",
-                "junit:target/xml-report/cucumber2.xml"}, // bu satir ile html,xml,json report olusturabiliyoruz
+        plugin={"html:target/cucumber-html-reports1/cucumber.html", // buradaki / \\ idi, degistirdim, emin degilim
+                "json:target/json-reports/cucumber1.json",
+                "junit:target/xml-report/cucumber1.xml"}, // bu satir ile html,xml,json report olusturabiliyoruz
         features = "src/test/resources/features",
         glue = {"stepdefinitions" , "hooks"},
-        tags = "@paralel2",
+        tags = "@paralel1",
         dryRun = false
 
         // step definition olusturmak icin calistirdiginda burayı true yapmayi unutma
 )
 
-public class ParalelTestRunner2
-{
+
+public class ParalelTestrunner1 {
+
 }
--- src/test/java/runners/ParalelTestrunner2.java
+++ src/test/java/runners/ParalelTestrunner2.java
@@ -9,18 +9,17 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(
-        plugin={"html:target/cucumber-html-reports1/cucumber.html", // buradaki / \\ idi, degistirdim, emin degilim
-                "json:target/json-reports/cucumber1.json",
-                "junit:target/xml-report/cucumber1.xml"}, // bu satir ile html,xml,json report olusturabiliyoruz
+        plugin={"html:target/cucumber-html-reports2/cucumber.html", // buradaki / \\ idi, degistirdim, emin degilim
+                "json:target/json-reports/cucumber2.json",
+                "junit:target/xml-report/cucumber2.xml"}, // bu satir ile html,xml,json report olusturabiliyoruz
         features = "src/test/resources/features",
         glue = {"stepdefinitions" , "hooks"},
-        tags = "@paralel1",
+        tags = "@paralel2",
         dryRun = false
 
         // step definition olusturmak icin calistirdiginda burayı true yapmayi unutma
 )
 
-
-public class ParalelTestRunner1 {
-
+public class ParalelTestrunner2
+{
 }
